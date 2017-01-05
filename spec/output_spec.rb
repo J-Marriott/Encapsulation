1afbde04c3a2e408e7d265694c8b56573edb4d3b
Joe Marriott <marriott90@gmail.com>
2017-01-05 12:40:58 +0000

    added multiply and divide functions

--- spec/output_spec.rb
+++ spec/output_spec.rb
@@ -1,7 +1,7 @@
 require 'calculator'
 
 describe Output do
-  it "prints a nice message" do
+  it "#pretty_print" do
     expect(subject.pretty_print(1)).to eq("The answer is 1")
     expect(subject.pretty_print(100)).to eq("The answer is 100")
   end
