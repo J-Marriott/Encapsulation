efe2c29e7d043da0bb6e6a05f03a87c04ff327b0
Joe Marriott <marriott90@gmail.com>
2017-01-05 11:55:58 +0000

    Classes for Calculator and Output defined

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,7 +1,15 @@
-def add(x,y)
-  x + y 
+class Calculator
+  def add(x,y)
+    @result = (x + y)
+  end
+
+  def subtract(x,y)
+    @result = (y - x)
+  end
 end
 
-def subtract(x,y)
-  y - x 
+class Output
+  def pretty_print(result)
+    "The answer is #{result}"
+  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,17 +1,20 @@
 require 'calculator'
 
-describe 'calculator' do
+describe Calculator do
   it "#add" do
-    expect(add(0,0)).to eq(0)
-    expect(add(0,1)).to eq(1)
+    expect(subject.add(0,0)).to eq(0)
+    expect(subject.add(0,1)).to eq(1)
   end
 
   it "#subtract" do
-    expect(subtract(1,1)).to eq(0)
-    expect(subtract(1,0)).to eq(-1)
+    expect(subject.subtract(1,1)).to eq(0)
+    expect(subject.subtract(1,0)).to eq(-1)
   end
+end
 
+describe Output do
   it "prints a nice message" do
-    expect(pretty_print(1)).to eq("The answer is 1")
+    expect(subject.pretty_print(1)).to eq("The answer is 1")
+    expect(subject.pretty_print(100)).to eq("The answer is 100")
   end
 end
